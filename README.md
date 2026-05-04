a9aa54a6792e88889aad8870e7cce2835df5f86a
Evgeniy Trukhmanov laptop <79169380904@mail.ru>
2026-05-04 23:33:56 +0300

    fix: database not connection

--- README.md
+++ README.md
@@ -6,12 +6,14 @@
 ---
 Для самостоятельного запуска проекта необходимо наличие [JDK 21](https://www.oracle.com/asean/java/technologies/downloads/#java21) и и [Apache Tomcat 11](https://tomcat.apache.org/download-11.cgi)
 ## Этапы запуска
-1) Сборка проекта
+1) Собрать проект
 ```shell
 ./gradlew clean war
 ```
 После успешной сборки в проекте появится артефакт .`/build/libs/ROOT.war`
+
 2) Удалить все из папки `{tomcat_home}/webapps` и переместить туда собранный артефакт
+
 3) Отредактировать конфигурационный файл `{tomcat_home}/conf/server.xml`.
 Нужно найти строки:
 ```xml
@@ -26,8 +28,9 @@
                redirectPort="8443"
                parseBodyMethods="POST,PUT,PATCH" />
 ```
+
 4) Остается запустить сервер:
 ```shell
 {tomcat_home}/bin/catalina.sh run
 ```
-Теперь сервер доступен по адресу `localhost:8080`
+Сервер доступен по адресу `localhost:8080`
--- src/main/java/ru/trukhmanov/util/DbHelper.java
+++ src/main/java/ru/trukhmanov/util/DbHelper.java
@@ -2,18 +2,25 @@ package ru.trukhmanov.util;
 
 import ru.trukhmanov.exception.DatabaseException;
 
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbHelper{
     private DbHelper(){}
-    private final static String CONNECTION_URL = "jdbc:sqlite:./src/main/resources/db/CurrencyConversion.db";
 
     public static Connection getConnection() {
         try{
+            String base = System.getProperty("catalina.base");
+            if (base == null || base.isBlank()) {
+                throw new DatabaseException("catalina.base is not set");
+            }
+            Path dbPath = Path.of(base, "webapps", "ROOT", "WEB-INF", "classes", "db", "CurrencyConversion.db");
+            String connectionUrl = "jdbc:sqlite:" + dbPath.toAbsolutePath();
+
             Class.forName("org.sqlite.JDBC");
-            var connection = DriverManager.getConnection(CONNECTION_URL);
+            var connection = DriverManager.getConnection(connectionUrl);
             connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
             connection.setAutoCommit(true);
             return connection;
