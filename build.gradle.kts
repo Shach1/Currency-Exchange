b6f5a6a4455dd5c3fd0a7c23f1bdd9988a8a2d28
Evgeniy Trukhmanov laptop <79169380904@mail.ru>
2026-06-07 19:20:36 +0300

    refactor: delete unused files and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,9 @@ repositories {
 
 dependencies {
     compileOnly("jakarta.platform:jakarta.jakartaee-web-api:11.0.0")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.xerial:sqlite-jdbc:3.49.1.0")
-    implementation("com.google.code.gson:gson:2.13.2")
     implementation("com.zaxxer:HikariCP:7.0.2")
+    implementation("com.google.code.gson:gson:2.13.2")
 }
 
 tasks.test {
